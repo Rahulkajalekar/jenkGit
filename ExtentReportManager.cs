70163fb4098631eb5e3e7d1856578df137ae6efc
rahul kajalekar <rahulkajalekar11@gmail.com>
2025-08-14 23:43:24 +0530

    Add project files.

---
+++ ExtentReportManager.cs
@@ -0,0 +1,51 @@
+﻿
+
+using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+
+namespace GithubActionsTestProj
+{
+    public class ExtentReportManager
+    {
+
+        public static ExtentReports extent;
+        public static ExtentTest test;
+
+
+
+        public static void createReport()
+        {
+
+            string outputDir = Path.Combine(Directory.GetCurrentDirectory(), "TestResults");
+            Directory.CreateDirectory(outputDir);
+
+            var htmlReporter = new ExtentSparkReporter(Path.Combine(outputDir, "ExtentReport.html"));
+            extent = new ExtentReports();
+            extent.AttachReporter(htmlReporter);
+
+
+
+
+
+
+        }
+
+
+        public static void FlushReport()
+        {
+            extent.Flush();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ UnitTest1.cs
@@ -0,0 +1,82 @@
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Interactions;
+using SeleniumExtras.WaitHelpers;
+
+namespace GithubActionsTestProj
+{
+    public class Tests
+    {
+        
+
+        IWebDriver driver;
+
+
+        [OneTimeSetUp]
+        public void OneTimeSetup()
+        {
+            ExtentReportManager.createReport();
+        }
+
+
+        [SetUp]
+        public void Setup()
+        {
+
+            ExtentReportManager.test = ExtentReportManager.extent.CreateTest(TestContext.CurrentContext.Test.Name);
+            var options = new ChromeOptions();
+            //options.AddArgument("--headless");
+            //options.AddArgument("--no-sandbox");
+            //options.AddArgument("--disable-gpu");
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl("https://playwright.dev");
+            
+        }
+
+        [Test]
+        public void verifyPalywrightPage()
+        {
+
+            OpenQA.Selenium.Support.UI.WebDriverWait wait = new OpenQA.Selenium.Support.UI.WebDriverWait(driver,TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.ElementIsVisible(By.XPath("//a[contains(text(),'Get started')]")));
+            IWebElement getStarted = driver.FindElement(By.XPath("//a[contains(text(),'Get started')]"));
+
+            if (getStarted.Text.ToUpper().Equals("GET STARTE"))
+            {
+                ExtentReportManager.test.Pass("Title contains GET STARTED");
+            }
+            else
+            {
+                ExtentReportManager.test.Fail("Title does not contain GET STARTED");
+            }
+            Assert.IsTrue(getStarted.Displayed,"Get Started Not Found.");
+        }
+
+
+        [TearDown]
+        public void tearDown()
+        {
+
+            driver.Quit();
+            var status = TestContext.CurrentContext.Result.Outcome.Status;
+            if (status == NUnit.Framework.Interfaces.TestStatus.Failed)
+            {
+                ExtentReportManager.test.Fail("Test failed: " + TestContext.CurrentContext.Result.Message);
+            }
+        }
+
+
+        [OneTimeTearDown]
+        public void TearDownReport()
+        {
+            ExtentReportManager.FlushReport();
+        }
+
+
+
+
+
+    }
+}
