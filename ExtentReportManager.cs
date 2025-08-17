cb6dc6773d287bc55c5fc0654d8c6454bde23e17
rahulkajalekar <rahulkajalekar11@gmail.com>
2025-08-17 17:36:34 +0530

    New Report

--- ExtentReportManager.cs
+++ ExtentReportManager.cs
@@ -12,16 +12,31 @@ namespace GithubActionsTestProj
 
         public static void CreateReport()
         {
-            if (extent == null) // make sure we don’t recreate
+            if (extent == null)
             {
                 string outputDir = Path.Combine(Directory.GetCurrentDirectory(), "TestResults");
                 Directory.CreateDirectory(outputDir);
 
-                string reportFile = Path.Combine(outputDir, $"ExtentReport_{DateTime.Now:yyyyMMdd_HHmmss}.html");
-                var htmlReporter = new ExtentSparkReporter(reportFile);
+                string timestampedReport = Path.Combine(outputDir, $"ExtentReport_{DateTime.Now:yyyyMMdd_HHmmss}.html");
+                string latestReport = Path.Combine(outputDir, "ExtentReport_LATEST.html");
 
+                var htmlReporter = new ExtentSparkReporter(timestampedReport);
                 extent = new ExtentReports();
                 extent.AttachReporter(htmlReporter);
+
+                var newhtmlreporter = new ExtentSparkReporter(latestReport);
+                extent.AttachReporter(newhtmlreporter);
+
+                //// Also copy the same report as LATEST.html after flush
+                //AppDomain.CurrentDomain.ProcessExit += (s, e) =>
+                //{
+                //    try
+                //    {
+                //        if (File.Exists(timestampedReport))
+                //            File.Copy(timestampedReport, latestReport, true);
+                //    }
+                //    catch { }
+                //};
             }
         }
 
